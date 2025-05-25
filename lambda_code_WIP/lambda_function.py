023cd412211a396cdb9737ff4b787f3d67a4d96c
John Doe <johndoe@atotallyrealcompany.com>
2025-05-25 12:24:47 +0000

    Added experimental Lambda code for my super secret project

---
+++ lambda_code_WIP/lambda_function.py
@@ -0,0 +1,24 @@
+import subprocess
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def run_command(command):
+    try:
+        logger.info("Running shell command: \"{}\"".format(command))
+        result = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+        logger.info("Command output:\n---\n{}\n---".format(result.stdout.decode('UTF-8')))
+    except Exception as e:
+        logger.error("Exception: {}".format(e))
+        return False
+
+    return True
+
+def lambda_handler(event, context):
+    # I've added a layer to the Lambda function that includes the awscli package.
+    # This allows us to run shell commands like 'aws s3' directly, for future functionality I haven't of yet.
+    # It is installed under the /opt directory in the Lambda environment.
+    
+    domain = event.get('domain')
+    run_command('nslookup' + domain)
